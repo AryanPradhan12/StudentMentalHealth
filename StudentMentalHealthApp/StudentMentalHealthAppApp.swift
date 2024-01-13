a513b99394aa596a59f17c435f164785cad27933
AryanPradhan <Aryanrp2007@gmail.com>
2024-01-13 15:31:12 +0400

    Initial Commit

---
+++ StudentMentalHealthApp/StudentMentalHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentMentalHealthAppApp.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StudentMentalHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
