359587c9a4be57236b9a56c1aa9a6883d76081e8
AryanPradhan <Aryanrp2007@gmail.com>
2024-01-25 18:47:18 +0400

    Progress in homepage

--- StudentMentalHealthApp/Homepage/Views/HomePageView.swift
+++ StudentMentalHealthApp/Homepage/Views/HomePageView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        Text("Dashboard")
+        NavigationStack {
+            ZStack {
+                Color("BkColor")
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Home Page")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.red)
+                
+                    List {
+                        Section("Features") {
+                            NavigationLink(destination: ToDoListView()) {
+                                
+                                Text("To Do List")}
+                        }
+                    }
+                    .foregroundColor(.red)
+                    .fontWeight(.bold)
+                    .padding()
+                    .background(Color.green)
+                }
+            }
+        }
     }
 }
-
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
--- StudentMentalHealthApp/Login&signup/Views/MainView.swift
+++ StudentMentalHealthApp/Login&signup/Views/MainView.swift
@@ -10,15 +10,26 @@ import SwiftUI
 struct MainView: View {
     @StateObject var viewModel = MainViewViewModel()
     var body: some View {
-        if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            //Signed in state
-            HomePageView()
-        } else {
-            LoginView()
+            if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
+                //Signed in state
+                TabView {
+                    
+                    HomePageView()
+                        .tabItem {
+                            Label("Home", systemImage: "house")
+                        }
+                    ProfileView()
+                        .tabItem {
+                            Label("Profile", systemImage: "person.circle")
+                        }
+                }
+            } else {
+                LoginView()
+            }
         }
-            
-    }
-}
+    
+        }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
