416e460824c42b1dea43c4b7f8a45bf32d44f4a0
AryanPradhan <Aryanrp2007@gmail.com>
2024-03-11 00:41:53 +0400

    Push notification function completed, design of page to be started.

---
+++ StudentMentalHealthApp/Homepage/Views/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 10/03/2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
