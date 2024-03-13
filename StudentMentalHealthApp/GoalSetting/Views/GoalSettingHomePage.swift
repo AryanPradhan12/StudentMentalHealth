e94d8a723a9643b1504bbfbbe943736a60c518cc
AryanPradhan <Aryanrp2007@gmail.com>
2024-03-13 18:50:12 +0400

    changed aesthetics of app.

---
+++ StudentMentalHealthApp/GoalSetting/Views/GoalSettingHomePage.swift
@@ -0,0 +1,18 @@
+//
+//  GoalSettingHomePage.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 13/03/2024.
+//
+
+import SwiftUI
+
+struct GoalSettingHomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalSettingHomePage()
+}
