2618def06fe55c0adbb24666be33c514ebf3e261
AryanPradhan <Aryanrp2007@gmail.com>
2024-03-13 19:27:59 +0400

    "Get help" section started.

---
+++ StudentMentalHealthApp/GetHelp/Views/GetHelpView.swift
@@ -0,0 +1,18 @@
+//
+//  GetHelpView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 13/03/2024.
+//
+
+import SwiftUI
+
+struct GetHelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetHelpView()
+}
