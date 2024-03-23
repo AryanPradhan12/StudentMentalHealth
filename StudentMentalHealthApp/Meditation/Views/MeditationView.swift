ff0f2b16712addee80e85e1fa49af85215c7fec9
AryanPradhan <Aryanrp2007@gmail.com>
2024-03-23 14:42:11 +0400

    Meditation feature started and in progress

---
+++ StudentMentalHealthApp/Meditation/Views/MeditationView.swift
@@ -0,0 +1,18 @@
+//
+//  MeditationView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 23/03/2024.
+//
+
+import SwiftUI
+
+struct MeditationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeditationView()
+}
