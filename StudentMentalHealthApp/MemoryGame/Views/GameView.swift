02cf9027a45b40ae83a5d897ffa25454e23ee4a9
AryanPradhan <Aryanrp2007@gmail.com>
2024-05-20 00:44:21 +0400

    Memory game introduced

---
+++ StudentMentalHealthApp/MemoryGame/Views/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 20/05/2024.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
