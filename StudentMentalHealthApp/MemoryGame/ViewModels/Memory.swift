0f671731970b251ce4418b771b1bc2bc35723499
AryanPradhan <Aryanrp2007@gmail.com>
2024-05-23 17:12:49 +0400

    Game complete

---
+++ StudentMentalHealthApp/MemoryGame/ViewModels/Memory.swift
@@ -0,0 +1,14 @@
+//
+//  GameView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 20/05/2024.
+//
+
+import Foundation
+import SwiftUI
+
+class Card:Identifiable, ObservableObject {
+    var id = UUID()
+    
+}
--- StudentMentalHealthApp/MemoryGame/Views/CardView.swift
+++ StudentMentalHealthApp/MemoryGame/Views/CardView.swift
@@ -1,5 +1,5 @@
 //
-//  GameView.swift
+//  CardView.swift
 //  StudentMentalHealthApp
 //
 //  Created by Aryan Pradhan on 23/05/2024.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct GameView: View {
+struct CardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    GameView()
+    CardView()
 }
