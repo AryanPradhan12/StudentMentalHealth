c74f645fcbfc961a82e2a99ad4a3852815653c37
AryanPradhan <Aryanrp2007@gmail.com>
2024-06-21 18:02:27 +0400

    Backgrounds for study and motivation hub changing in progress

--- StudentMentalHealthApp/MotivationHub/Views/MotivationView.swift
+++ StudentMentalHealthApp/MotivationHub/Views/MotivationView.swift
@@ -13,47 +13,54 @@ struct MotivationView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.homepagetextcolor
-                    .ignoresSafeArea()
-                VStack {
-                    if isVisible {
-                        Text("Welcome to the Motivation HUB!")
-                            .bold()
-                            .font(.system(size: 25))
-                            .animation(.easeInOut(duration: 1.0))
-                        Spacer()
+            GeometryReader { geo in
+                ZStack {
+                    Image("Meditationimage")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                        .opacity(0.7)
+                    
+                    VStack {
+                        if isVisible {
+                            Text("Welcome to the Motivation HUB!")
+                                .bold()
+                                .font(.system(size: 25))
+                                .animation(.easeInOut(duration: 1.0))
+                            Spacer()
+                        }
                     }
-                }
-                .onAppear {
-                    withAnimation {
-                        self.isVisible = true
+                    .onAppear {
+                        withAnimation {
+                            self.isVisible = true
+                        }
                     }
+                    VStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.blue)
+                            .overlay(
+                                NavigationLink(
+                                    destination: GoalListView(),
+                                    label: {
+                                        VStack {
+                                            Image("GoalSettingIcon")
+                                                .resizable()
+                                                .frame(width: 55, height: 50)
+                                                .foregroundColor(.white)
+                                            Text("Goal Setting")
+                                                .foregroundColor(.white)
+                                                .fontWeight(.semibold)
+                                                .font(.system(size: 15))
+                                        }
+                                    }))
+                    }
+                    .offset(x: -75, y: -250)
                 }
-                VStack {
-                    RoundedRectangle(cornerRadius: 15)
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.blue)
-                        .overlay(
-                            NavigationLink(
-                                destination: GoalListView(),
-                                label: {
-                                    VStack {
-                                        Image("GoalSettingIcon")
-                                            .resizable()
-                                            .frame(width: 55, height: 50)
-                                            .foregroundColor(.white)
-                                        Text("Goal Setting")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.semibold)
-                                            .font(.system(size: 15))
-                                    }
-                                }))
-                }
-                .offset(x: -90, y: -250)
+                
             }
-            
-            }
+        }
         }
     }
 
