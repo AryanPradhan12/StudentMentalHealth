90a6bd97c61c4cc51ae3014e1694407d2383d3fa
AryanPradhan <Aryanrp2007@gmail.com>
2024-06-24 18:25:10 +0400

    Rate your day feature added.

---
+++ StudentMentalHealthApp/RateYourDay/Views/RateYourDayView.swift
@@ -0,0 +1,18 @@
+//
+//  RateYourDayView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 24/06/2024.
+//
+
+import SwiftUI
+
+struct RateYourDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RateYourDayView()
+}
