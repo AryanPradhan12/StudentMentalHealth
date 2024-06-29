f15f8f59fd87fed8fd5e5178aaf24730cd1c5322
AryanPradhan <Aryanrp2007@gmail.com>
2024-06-29 15:37:19 +0400

    StudyHub song playlist started

---
+++ StudentMentalHealthApp/StudyHub/ViewModels/StudyPlaylistViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StudyPlaylistViewModel.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 29/06/2024.
+//
+
+import Foundation
---
+++ StudentMentalHealthApp/StudyHub/Views/StudyPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  StudyPlaylistView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 29/06/2024.
+//
+
+import SwiftUI
+
+struct StudyPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudyPlaylistView()
+}
