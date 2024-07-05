fd330ffbc881c3f100266886148ecf56f9fcc799
AryanPradhan <Aryanrp2007@gmail.com>
2024-07-05 17:10:54 +0530

    Almost done

---
+++ StudentMentalHealthApp/GetHelp/Views/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  StudentMentalHealthApp
+//
+//  Created by Aryan Pradhan on 05/07/2024.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
